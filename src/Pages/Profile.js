78a38263e1f663923582b0540036b10711047e91
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2022-06-12 18:34:19 -0400

    added formik hook to profile page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,19 +1,63 @@
-import { React, useState } from "react";
+import { React, useState, useContext } from "react";
+import { UserContext } from "../Utilities/UserContext";
+import { useFormik } from "formik";
 
 function Profile() {
   const [editing, setEditing] = useState(false);
-  if (!editting) {
+  const [user, setUser] = useContext(UserContext);
+
+  const toggleEdit = () => {
+    setEditing(!editing);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      riotId: "",
+      description: "",
+      email: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+      toggleEdit();
+    },
+  });
+
+  if (!editing) {
     return (
       <div>
+        <h1>{user["username"]}</h1>
         <h1>Profile</h1>
-        <button>EDIT PROFILE</button>
+        <button onClick={toggleEdit}>EDIT PROFILE</button>
       </div>
     );
   } else {
     return (
       <div>
+        <h1>{user["username"]}</h1>
+
         <h1>Editing Profile</h1>
-        <button>SAVE PROFILE</button>
+
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="riotId">Riot ID</label>
+          <input
+            id="riotId"
+            name="riotId"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.riotId}
+          />
+
+          <label htmlFor="description">Description</label>
+          <input
+            id="description"
+            name="description"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.description}
+          />
+
+          <button type="submit">SAVE PROFILE</button>
+        </form>
       </div>
     );
   }
