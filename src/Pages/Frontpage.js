b148f17f1f9b659852dcb4443c911b20d70c76c6
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2022-06-12 22:42:19 -0400

    Cleaned up discord auth flow code for frontend

--- src/Pages/Frontpage.js
+++ src/Pages/Frontpage.js
@@ -1,6 +1,7 @@
 import { React, useContext, useEffect, useRef } from "react";
 import { ref, set } from "firebase/database";
 import { UserContext } from "../Utilities/UserContext";
+import GetDiscordData from "../Utilities/GetDiscordData";
 import "./Frontpage.css";
 // import databaseRef from "../Utilities/firebase";
 
@@ -15,50 +16,21 @@ function Frontpage(props) {
     if (url.includes("?") && !loggedIn.current) {
       const params = new URL(url);
       const code = params.searchParams.get("code");
-      const resp = DiscordPostRequest(code);
+      const resp = GetDiscordData(
+        code,
+        REACT_APP_CLIENT_ID,
+        REACT_APP_SECRET,
+        setUser,
+        loggedIn
+      );
       console.log(resp);
     }
   }, []);
 
-  const DiscordPostRequest = async (code) => {
-    console.log(code, REACT_APP_CLIENT_ID, REACT_APP_SECRET);
-    const data_1 = new URLSearchParams();
-    data_1.append("client_id", REACT_APP_CLIENT_ID);
-    data_1.append("client_secret", REACT_APP_SECRET);
-    data_1.append("grant_type", "authorization_code");
-    data_1.append("redirect_uri", `http://localhost:3000`);
-    data_1.append("scope", "identify");
-    data_1.append("code", code);
-    await fetch("https://discord.com/api/oauth2/token", {
-      method: "POST",
-      body: data_1,
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        GetUserData(data);
-      });
-  };
-  const GetUserData = async (resp) => {
-    let options = {
-      url: "https://discordapp.com/api/users/@me",
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + resp["access_token"],
-      },
-    };
-    var userResponse = await fetch(
-      "https://discordapp.com/api/users/@me",
-      options
-    );
-    const userData = await userResponse.json();
-    console.log(userData);
-    setUser(userData);
-  };
-
   return (
     <div className="container">
       <h1 className="title">A SUBTITLE </h1>
-      {/* <h2>hello {JSON.stringify(user)}</h2> */}
+      <h2>hello {JSON.stringify(user)}</h2>
       <div className="renderContainer"></div>
     </div>
   );
---
+++ src/Utilities/GetDiscordData.js
@@ -0,0 +1,46 @@
+export default function GetDiscordData(
+  code,
+  REACT_APP_CLIENT_ID,
+  REACT_APP_SECRET,
+  setUser,
+  loggedIn
+) {
+  const DiscordPostRequest = async () => {
+    console.log(code, REACT_APP_CLIENT_ID, REACT_APP_SECRET);
+    const data_1 = new URLSearchParams();
+    data_1.append("client_id", REACT_APP_CLIENT_ID);
+    data_1.append("client_secret", REACT_APP_SECRET);
+    data_1.append("grant_type", "authorization_code");
+    data_1.append("redirect_uri", `http://localhost:3000`);
+    data_1.append("scope", "identify");
+    data_1.append("code", code);
+    await fetch("https://discord.com/api/oauth2/token", {
+      method: "POST",
+      body: data_1,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        GetUserData(data);
+      });
+  };
+  const GetUserData = async (resp) => {
+    if (loggedIn.current) {
+      return;
+    }
+    let options = {
+      url: "https://discordapp.com/api/users/@me",
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + resp["access_token"],
+      },
+    };
+    var userResponse = await fetch(
+      "https://discordapp.com/api/users/@me",
+      options
+    );
+    const userData = await userResponse.json();
+    console.log(userData);
+    setUser(userData);
+  };
+  DiscordPostRequest();
+}
